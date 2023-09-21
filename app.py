884b9d0691936587be95d92195b2eb50a77eb84e
romi-sai <54590905+romi-sai@users.noreply.github.com>
2023-09-21 00:04:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,15 @@ st.subheader("Hi I am Romika! :partying_face:")
 st.write("A student from the University at Buffalo, pursuing my MS in AI :sign_of_the_horns:")
 st.write("This website checks for deadlinks upto five depths for each link found on the website you are trying to check.")
 st.write("This website was created using the tutorial [here](https://www.youtube.com/watch?v=VqgUkExPvLY&t=35s).")
-
-# Create a text input widget for the user to enter the URL
-
-# Create a button to initiate the link checking
-
 st.title("Dead Link Checker (Depth = 5)")
 
-visited_links = set()  # set to keep track of visited links
+visited_links = set() 
 
 def check_links(url, depth=0):
-    if depth >= 5:  # maximum depth reached
+    if depth >= 5:
         return
 
-    if url in visited_links:  # link already visited, skip
+    if url in visited_links: 
         return
     visited_links.add(url)
 
